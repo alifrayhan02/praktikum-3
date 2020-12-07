7faa23fffedd8e7b147a39688c85c07e78ba6784
unknown <alif_rayhan_27rpl@student.smktelkom-mlg.sch.id>
2020-12-07 21:37:51 +0700

    praktikum3

---
+++ react/src/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Pegawai from "./Page/Pegawai";
+
+class Main extends Component {
+  render = () => {
+    return (
+      <Switch>
+        <Route path="/pegawai" component={Pegawai} />
+      </Switch>
+    );
+  };
+}
+
+export default Main;
